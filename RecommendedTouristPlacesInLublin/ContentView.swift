ea5cfa130448ab199668e87d152ff33bc7a46afe
xStck <dawidn392@gmail.com>
2022-05-15 17:16:25 +0200

    Empty_project

---
+++ RecommendedTouristPlacesInLublin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecommendedTouristPlacesInLublin
+//
+//  Created by Dawid Nalepa on 15/05/2022.
+//  Copyright © 2022 NalepaDawid_OgorzalekDaniel_OleszkoTomasz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
