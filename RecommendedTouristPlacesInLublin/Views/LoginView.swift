8476c980bc57308f0ee8d5185ff8c9c5c8ee3173
Tomasz <tomekoleszko@gmail.com>
2022-05-17 14:46:04 +0200

    Adding places refactoring

--- RecommendedTouristPlacesInLublin/Views/LoginView.swift
+++ RecommendedTouristPlacesInLublin/Views/LoginView.swift
@@ -95,36 +95,22 @@ struct LoginView: View {
             }
             
             //Dodawanie do bazy danych zabytków
-            var placeNameArr: [String] = ["Pałac Sobieskich", "Archikatedra św. Jana Chrzciciela i św. Jana Ewangelisty", "Baszta Gotycka"]
-            var placeDescArr: [String] = ["Pałac Sobieskich (Radziwiłłów, Vetterów) został wybudowany w pierwszej połowie XVI wieku. Pierwotnie służył jako punkt obronny wysunięty poza mury miasta", "Ufundowana pod koniec XVI wieku katedra o barokowym wnętrzu ozdobionym freskami.", "Baszta Półokrągła zwana jest tak z racji swojego kształtu. Jest ona wraz z fragmentem murów obronnych, świadectwem gotyckich obwarowań staromiejskich."]
-            var placeLongitudeArr: [String] = ["51.24513945062349","51.246899237466", "51.24686560691351"]
-            var placeLattitudeArr: [String] = ["22.56628734638244", "22.568356426400396", "22.566617811484523"]
-            addCategoryItemsToDB(viewContext: viewContext, categoryName: "Zabytki", placeNameArr: placeNameArr, placeDescArr: placeDescArr, placeLongitudeArr: placeLongitudeArr, placeLattitudeArr: placeLattitudeArr)
-            
-            //Dodawanie do bazy danych restauracji
-            placeNameArr = ["Koper Włoski u Braci Mazur", "Restauracja Magia w Lublinie", "Pyzata Chata"]
-            placeDescArr = ["Dania podawane na wspólnych talerzach i koktajle w bezpretensjonalnej restauracji włoskiej z tarasem.", "Tradycyjna restauracja serwująca sycące dania, tatary i pizze oraz koktajle, wina i mocne alkohole.", "Bezpretensjonalna, przytulna restauracja serwująca duże porcje takich potraw jak pierogi, kotlety i zupy."]
-            placeLongitudeArr = ["51.23755448979576", "51.24841909740968", "51.24551039653255"]
-            placeLattitudeArr = ["22.548470750409923", "22.567890389713853", "22.556392642409673"]
-            addCategoryItemsToDB(viewContext: viewContext, categoryName: "Restauracje", placeNameArr: placeNameArr, placeDescArr: placeDescArr, placeLongitudeArr: placeLongitudeArr, placeLattitudeArr: placeLattitudeArr)
-            
-            
-            
-            //Dodawanie do bazy danych kin
-            placeNameArr = ["Multikino","Cinema City","Kino Perła"]
-            placeDescArr = ["Multikino w Lublinie zostało otwarte w listopadzie 2013 roku. Mieści się na pierwszym piętrze w Galerii Olimp.","Cinema City Felicity składa się z 9 klimatyzowanych sal, które pomieszczą jednocześnie blisko 1 700 widzów.","Funkcjonuje w sezonie wakacyjnym, od czerwca do sierpnia 5 dni w tygodniu, od środy do niedzieli wyświetlamy filmowe hity. Projekcje zaczynają się po zachodzie słońca."]
-            placeLongitudeArr = ["51.26736717016585","51.23210987725407","51.243974482797086"]
-            placeLattitudeArr = ["22.57117602346184","22.614365544970106","22.567327857030296"]
-            addCategoryItemsToDB(viewContext: viewContext, categoryName: "Kina", placeNameArr: placeNameArr, placeDescArr: placeDescArr, placeLongitudeArr: placeLongitudeArr, placeLattitudeArr: placeLattitudeArr)
-            
-            
-            //Dodawanie do bazy danych teatrów
-            placeNameArr = ["Teatr ITP","Teatr Pierwszego Kontaktu","Teatr im. Hansa Christiana Andersena w Lublinie"]
-            placeDescArr = ["Zespół został założony z inicjatywy salezjanina, ks. Mariusza Lacha w 2001 roku.","Teatr Pierwszego Kontaktu powstał w październiku 2005 roku w Lublinie. Pierwsza grupa wyłoniła się w wyniku warsztatów prowadzonych przez Henryka Kowalczyka – założyciela Teatru Scena 6, które odbywały się w Wojewódzkim Ośrodku Kultury.","Teatr im. H. Ch. Andersena jest miejscem, w którym to, co piękne i ciekawe płynnie przenika się z tym, co trudne i skłaniające do refleksji."]
-            placeLongitudeArr = ["51.24819170076517","51.235935547887976","51.24678149283614"]
-            placeLattitudeArr = ["22.544951139880023","22.504817079571794","22.548075803148798"]
-            addCategoryItemsToDB(viewContext: viewContext, categoryName: "Teatry", placeNameArr: placeNameArr, placeDescArr: placeDescArr, placeLongitudeArr: placeLongitudeArr, placeLattitudeArr: placeLattitudeArr)
-            
+            var categoryNameArr = ["Zabytki","Restauracje","Kina","Teatry"] 
+            var placeNameArr: [String] = ["Pałac Sobieskich", "Archikatedra św. Jana Chrzciciela i św. Jana Ewangelisty", "Baszta Gotycka", "Koper Włoski u Braci Mazur", "Restauracja Magia w Lublinie", "Pyzata Chata", "Multikino", "Cinema City", "Kino Perła", "Teatr ITP", "Teatr Pierwszego Kontaktu", "Teatr im. Hansa Christiana Andersena w Lublinie"]
+            var placeDescArr: [String] = ["Pałac Sobieskich (Radziwiłłów, Vetterów) został wybudowany w pierwszej połowie XVI wieku. Pierwotnie służył jako punkt obronny wysunięty poza mury miasta", "Ufundowana pod koniec XVI wieku katedra o barokowym wnętrzu ozdobionym freskami.", "Baszta Półokrągła zwana jest tak z racji swojego kształtu. Jest ona wraz z fragmentem murów obronnych, świadectwem gotyckich obwarowań staromiejskich.", "Dania podawane na wspólnych talerzach i koktajle w bezpretensjonalnej restauracji włoskiej z tarasem.", "Tradycyjna restauracja serwująca sycące dania, tatary i pizze oraz koktajle, wina i mocne alkohole.", "Bezpretensjonalna, przytulna restauracja serwująca duże porcje takich potraw jak pierogi, kotlety i zupy.", "Multikino w Lublinie zostało otwarte w listopadzie 2013 roku. Mieści się na pierwszym piętrze w Galerii Olimp.", "Cinema City Felicity składa się z 9 klimatyzowanych sal, które pomieszczą jednocześnie blisko 1 700 widzów.", "Funkcjonuje w sezonie wakacyjnym, od czerwca do sierpnia 5 dni w tygodniu, od środy do niedzieli wyświetlamy filmowe hity. Projekcje zaczynają się po zachodzie słońca.", "Zespół został założony z inicjatywy salezjanina, ks. Mariusza Lacha w 2001 roku.", "Teatr Pierwszego Kontaktu powstał w październiku 2005 roku w Lublinie. Pierwsza grupa wyłoniła się w wyniku warsztatów prowadzonych przez Henryka Kowalczyka – założyciela Teatru Scena 6, które odbywały się w Wojewódzkim Ośrodku Kultury.", "Teatr im. H. Ch. Andersena jest miejscem, w którym to, co piękne i ciekawe płynnie przenika się z tym, co trudne i skłaniające do refleksji."]
+            var placeLongitudeArr: [String] = ["51.24513945062349", "51.246899237466", "51.24686560691351", "51.23755448979576", "51.24841909740968", "51.24551039653255", "51.26736717016585", "51.23210987725407", "51.243974482797086", "51.24819170076517", "51.235935547887976", "51.24678149283614"]
+            var placeLattitudeArr: [String] = ["22.56628734638244", "22.568356426400396", "22.566617811484523", "22.548470750409923", "22.567890389713853", "22.556392642409673", "22.57117602346184", "22.614365544970106", "22.567327857030296", "22.544951139880023", "22.504817079571794", "22.548075803148798"]
+
+            categoryNameArr.forEach { category in 
+                //dodawanie miejsc w zależności od kategorii 
+                addCategoryItemsToDB(viewContext: viewContext, categoryName: category, placeNameArr: Array(placeNameArr.prefix(3)), placeDescArr: Array(placeDescArr.prefix(3)), placeLongitudeArr: Array(placeLongitudeArr.prefix(3)), placeLattitudeArr: Array(placeLattitudeArr.prefix(3)))
+
+                //Inna kategoria co 3 miejsca
+                placeNameArr.dropFirst(3)
+                placeDescArr.dropFirst(3)
+                placeLongitudeArr.dropFirst(3)
+                placeLattitudeArr.dropFirst(3)
+            }
             
             //Dodawanie opinii
             let rating: [Int16] = [5,4,3,5,5,5,4,5,4,5,3,2,5,4,3,5,5,5,5,5,5,5,5,5,4,5,4,3,3,1,5,4,5,5,5,1]
